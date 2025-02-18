ab21b41d88d9ea51d9d48d331577aea01fed9fab
Payam Meyer <payam.meyer@ascend-tech.us>
2025-02-18 14:08:37 -0500

    Removed readme, added user/pass text field and a little styling.

--- zulia-ui/src/components/LandingView.vue
+++ zulia-ui/src/components/LandingView.vue
@@ -4,6 +4,11 @@
       <div class="text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h2 font-weight-bold">Zulia UI!</h1>
+        <v-divider class="ma-10" />
+        <div class="pa-2">
+          <v-text-field label="Username" variant="solo" />
+          <v-text-field label="Password" variant="solo" />
+        </div>
       </div>
     </v-responsive>
   </v-container>
